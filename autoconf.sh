07dc8bc37cc084f7e53e754a5416dfd46c99f405
Michael Schober <mschober@BELC02KC1WLFFT3.sea.corp.expecn.com>
2013-05-07 08:24:05 -0700

    using polymorphism to make child scripts simple

--- autoconf.sh
+++ autoconf.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
 
-SCRIPT=$(BASENAME $0)
-AUTOCONF=autoconf-2.69
-ARCHIVES=$HOME/apps/archives
-LOCAL=$HOME/apps/usr/local
+APP=autoconf-2.69
+APP_TAR=$APP.tar.gz
+DOWNLOAD_COMMANDS="\
+  _CHECK_FOR_DOWNLOADED || wget http://ftp.gnu.org/gnu/autoconf/$APP_TAR
+"
+
+INSTALL_COMMANDS="\
+  tar xf $APP_TAR ;\
+  cd $APP ;\
+  _make --prefix=\$LOCAL
+"
 
 source compiler.func
 
-[[ $# -ne 1 ]] && echo "usage $SCRIPT -i or $SCRIPT -u" && exit 1
-
-function _download_autoconf {
-  test -f $ARCHIVES/$AUTOCONF.tar.gz && echo "$AUTOCONF.tar.gz already downloaded" && return
-  wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
-}
-
-function _install {
-  cd $ARCHIVES
-  _download_autoconf
-  tar xf $AUTOCONF.tar.gz
-  cd $ARCHIVES/$AUTOCONF
-  _make --prefix=$LOCAL
-}
-
-function _uninstall {
-  [[ ! -d $ARCHIVES/$AUTOCONF ]] && echo "$ARCHIVES/$AUTOCONF does not exist" && return
-  cd $ARCHIVES/$AUTOCONF
-  _break
-  cd ..
-  rm -rf $AUTOCONF
-}
-
-while getopts iu opt; do
-  case $opt in
-    i)
-      echo "installing $AUTOCONF"
-      #trap _uninstall SIGINT
-      _install
-      ;;
-    u)
-      echo "uninstalling $AUTOCONF"
-      #trap _uninstall SIGINT
-      _uninstall
-      ;;
-  esac
-done
+_USAGE $@
--- unixODBC.sh
+++ unixODBC.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
 
-SCRIPT=$(BASENAME $0)
-ODBC=unixODBC-2.3.1
-ARCHIVES=$HOME/apps/archives
-LOCAL=$HOME/apps/usr/local
+APP=unixODBC-2.3.1
+APP_TAR=$APP.tar.gz
+DOWNLOAD_COMMANDS="\
+  _CHECK_FOR_DOWNLOADED || wget ftp://ftp.unixodbc.org/pub/unixODBC/$APP_TAR
+"
+
+INSTALL_COMMANDS="\
+  tar xf $APP_TAR ;\
+  cd $APP ;\
+  _make --prefix=\$LOCAL
+"
 
 source compiler.func
 
-[[ $# -ne 1 ]] && echo "usage $SCRIPT -i or $SCRIPT -u" && exit 1
-
-function _download_odbc {
-  test -f $ARCHIVES/$ODBC.tar.gz && echo "$ODBC.tar.gz already downloaded" && return
-  wget ftp://ftp.unixodbc.org/pub/unixODBC/$ODBC.tar.gz
-}
-
-function _install {
-  cd $ARCHIVES
-  _download_odbc
-  tar xf $ODBC.tar.gz
-  cd $ARCHIVES/$ODBC
-  _make --prefix=$LOCAL
-}
-
-function _uninstall {
-  [[ ! -d $ARCHIVES/$ODBC ]] && echo "$ARCHIVES/$ODBC does not exist" && return
-  cd $ARCHIVES/$ODBC
-  _break
-  cd ..
-  rm -rf $ODBC
-}
-
-while getopts iu opt; do
-  case $opt in
-    i)
-      echo "installing $ODBC"
-      #trap _uninstall SIGINT
-      _install
-      ;;
-    u)
-      echo "uninstalling $ODBC"
-      #trap _uninstall SIGINT
-      _uninstall
-      ;;
-  esac
-done
+_USAGE $@
