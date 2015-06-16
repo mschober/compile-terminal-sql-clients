b4fa93eb87b422f5a8dfb76ff79a6690cf311aa4
Michael Schober <mynetlogins.schober@gmail.com>
2015-06-16 05:16:51 -0700

    Update freetds.sh
    versioned freetds tar

--- freetds.sh
+++ freetds.sh
@@ -2,8 +2,8 @@
 
 #_USAGE $@
 
-APP=freetds-0.91
-APP_TAR=freetds-stable.tgz
+APP=freetds-0.91.100
+APP_TAR=$APP.tar.gz
 DOWNLOAD_COMMANDS="\
   _CHECK_FOR_DOWNLOADED || wget ftp://ftp.freetds.org/pub/freetds/stable/$APP_TAR
 "
