ccf108f474a672b08401a12b72ff073d2196c7dd
Michael Schober <mynetlogins.schober@gmail.com>
2015-06-16 05:09:10 -0700

    Update freetds.sh
    hardcode the app/app_tar

--- freetds.sh
+++ freetds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-_USAGE $@
+#_USAGE $@
 
-APP=${1:-freetds-0.91}
-APP_TAR=${2:-freetds-stable.tgz}
+APP=freetds-0.91
+APP_TAR=freetds-stable.tgz
 DOWNLOAD_COMMANDS="\
   _CHECK_FOR_DOWNLOADED || wget ftp://ftp.freetds.org/pub/freetds/stable/$APP_TAR
 "
